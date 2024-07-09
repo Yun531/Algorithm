f220e4ac82276818b9ca2706723e0e4ac8a97ca2
Yun531 <74465803+Yun531@users.noreply.github.com>
2024-07-09 21:15:22 +0900

    [Bronze V] Title: A/B, Time: 224 ms, Memory: 18052 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Backjoon1008.solution();
+    }
+}
+
+class Backjoon1008 {
+    static double A, B;
+    public static void solution(){
+        input();
+        calc();
+        print();
+    }
+
+    public static void input(){
+        Scanner sc = new Scanner(System.in);
+
+        A = sc.nextDouble();
+        B = sc.nextDouble();
+
+        sc.close();
+    }
+
+    public static void calc(){
+        A = A/B;
+    }
+
+    public static void print(){
+        System.out.println(A);
+    }
+}
