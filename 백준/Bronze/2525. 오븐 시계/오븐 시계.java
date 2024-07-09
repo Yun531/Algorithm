0329599cf3902d778065bd595d4b9a1eb65cd336
Yun531 <74465803+Yun531@users.noreply.github.com>
2024-07-09 21:32:35 +0900

    [Bronze III] Title: 오븐 시계, Time: 232 ms, Memory: 18504 KB -BaekjoonHub

---
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Backjoon2525.solution();
+    }
+}
+
+class Backjoon2525 {
+    static int currentTime_h, currentTime_m, cookTime;
+    public static void solution(){
+        input();        //현재 시각, 요리 시간 입력
+        calc();         //완료 시간 계산
+        print();        //결과 출력
+    }
+
+    public static void input(){
+        Scanner sc = new Scanner(System.in);
+
+        currentTime_h = sc.nextInt();
+        currentTime_m = sc.nextInt();
+        cookTime = sc.nextInt();
+
+        sc.close();
+    }
+
+    public static void calc(){
+        int temp = currentTime_m + cookTime;
+        if(temp >= 60){
+            currentTime_h += temp / 60;
+            currentTime_m = temp % 60;
+            if(currentTime_h >= 24) currentTime_h -= 24;
+        }
+        else{
+            currentTime_m = temp;
+        }
+    }
+
+    public static void print(){
+        System.out.println(currentTime_h + " " + currentTime_m);
+    }
+}
