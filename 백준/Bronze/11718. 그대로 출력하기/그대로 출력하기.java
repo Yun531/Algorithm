d9da3283b3c22ebf180f0fc5082ff1277d9c5227
Yun531 <74465803+Yun531@users.noreply.github.com>
2024-07-10 20:30:06 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 192 ms, Memory: 17940 KB -BaekjoonHub

--- 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
@@ -12,21 +12,32 @@ interface Algorithm {
 
 }
 class Backjoon11718 implements Algorithm {
-
+    String[] test = new String[100];
     public void solution(){
+        input();
         print();
     }
-    
 
-    public void print(){
+
+    public void input(){
         Scanner sc = new Scanner(System.in);
 
+        int count = 0;
         while(sc.hasNextLine()) {
-            String str = sc.nextLine();
-
-            System.out.println(str);
+            test[count++] = sc.nextLine();
         }
         sc.close();
+    }
 
+//    public void print(){
+//        for(String s : test){                 해당 방법은 입력이 몇 줄이 입력되던 상관없이, 100줄 모두를 출력함으로 잘못된 풀이
+//            System.out.println(s);
+//        }
+//    }
+    public void print(){
+        for(int i = 0; i < test.length; i++){
+            if(test[i] == null) break;
+            System.out.println(test[i]);
+        }
     }
 }
