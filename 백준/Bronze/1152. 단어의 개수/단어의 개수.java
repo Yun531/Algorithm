3d7d3f1ae75cf70bfa81d475841972b4297b22d5
Yun531 <74465803+Yun531@users.noreply.github.com>
2024-07-11 19:29:46 +0900

    [Bronze II] Title: 단어의 개수, Time: 464 ms, Memory: 35488 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) {
+        Algorithm algorithm = new Backjoon1152();
+        algorithm.solution();
+    }
+}
+
+interface Algorithm {
+    public void solution();
+
+}
+class Backjoon1152 implements Algorithm{
+    public void solution(){
+        String in = input();
+        int count = calc(in);
+        print(count);
+    }
+
+    // 대소문자 공백 문자열 입력 > 몇 개의 단어
+    // 한 줄 입력
+    public String input(){
+        Scanner sc = new Scanner(System.in);
+        String temp = sc.nextLine();
+        sc.close();
+
+        return temp;
+    }
+
+    public int calc(String s){
+        int temp = 0;
+        StringTokenizer st = new StringTokenizer(s);
+
+        while(st.hasMoreTokens()){
+            st.nextToken();             //해당 상황에서 토근을 꺼내와서 할 작업은 없지만,
+            temp++;                     //토근을 꺼내줌으로서 제거를 해줘야 정상작동함
+        }
+
+        return temp;
+    }
+
+    public void print(int count){
+        System.out.println(count);
+    }
+}
