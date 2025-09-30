2425acc9ad641d7114923c8f743ac48592697080
김재윤 <74465803+Yun531@users.noreply.github.com>
2025-09-30 19:59:02 +0900

    [Silver II] Title: 가장 큰 증가하는 부분 수열, Time: 72 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+def sol():
+    n = int(input())
+    arr = list(map(int, input().split()))
+    memo = arr[:]
+
+    for i in range(1,n):
+        for k in range(i):
+            if arr[i] > arr[k]:
+                memo[i] = max(memo[i], memo[k]+arr[i])
+
+    print(max(memo))
+
+sol()
