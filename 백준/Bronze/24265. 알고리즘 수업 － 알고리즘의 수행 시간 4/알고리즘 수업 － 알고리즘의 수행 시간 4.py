66f7bed18d6d0bfe00e05b1b56f4eed41c958db4
김재윤 <74465803+Yun531@users.noreply.github.com>
2025-10-01 11:42:24 +0900

    Update 알고리즘 수업 － 알고리즘의 수행 시간 4.py

--- 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.py
+++ 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.py
@@ -5,7 +5,8 @@ input = sys.stdin.readline
 
 def sol():
     n = int(input())
-    print(int(n*(n-1)/2))
+    print(int(n*(n-1)/2)) # '/' 연산은 실수를 생성하기 때문에. int()로 감싸주거나 '//' 연산을 사용해야 함
     print(2)
+
 
 sol()
