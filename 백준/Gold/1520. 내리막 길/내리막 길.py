1f9e8197b9ec9ec3685bde269fd951a907a6b63b
김재윤 <74465803+Yun531@users.noreply.github.com>
2025-10-12 14:24:55 +0900

    [Gold III] Title: 내리막 길, Time: 116 ms, Memory: 45972 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.py
+++ 백준/Gold/1520. 내리막 길/내리막 길.py
@@ -2,8 +2,9 @@
 
 import sys
 input = sys.stdin.readline
+sys.setrecursionlimit(10 ** 9)  # 재귀 호출 허용 깊이를 변경
 
-def sol():
+def sol():   
     N, M = map(int, input().strip().split())
     input_list = [list(map(int, input().strip().split())) for _ in range(N)]
 
@@ -27,4 +28,34 @@ def sol():
     
     print(memo[N-1][M-1])
 
-sol()
+
+
+## sol_2
+N, M = map(int, input().strip().split())
+input_list = [list(map(int, input().strip().split())) for _ in range(N)]
+
+memo = [[-1]*M for _ in range(N)]
+
+DI = [0,0,1,-1]
+DK = [1,-1,0,0]
+
+def dfs(i, k):
+    if i == N-1 and k == M-1:
+        return 1
+    if memo[i][k] != -1:
+        return memo[i][k]
+    
+    ways = 0
+    for d in range(4):
+        ni, nk = i + DI[d], k + DK[d]
+        if 0 <= ni < N and 0 <= nk < M and input_list[i][k] > input_list[ni][nk]:
+            ways += dfs(ni,nk)
+    
+    memo[i][k] = ways
+
+    return memo[i][k]
+
+def sol_2():
+    print(dfs(0,0))
+
+sol_2()
