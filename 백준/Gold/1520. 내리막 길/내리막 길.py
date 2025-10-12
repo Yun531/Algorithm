173e0326fea071cba3ffabebe1b29670057ec310
김재윤 <74465803+Yun531@users.noreply.github.com>
2025-10-12 13:32:30 +0900

    [Gold III] Title: 내리막 길, Time: 332 ms, Memory: 66560 KB -BaekjoonHub

---
+++ 백준/Gold/1520. 내리막 길/내리막 길.py
@@ -0,0 +1,30 @@
+# https://www.acmicpc.net/problem/1520
+
+import sys
+input = sys.stdin.readline
+
+def sol():
+    N, M = map(int, input().strip().split())
+    input_list = [list(map(int, input().strip().split())) for _ in range(N)]
+
+    memo = [[0]*M for _ in range(N)]
+    memo[0][0] = 1
+
+    H = [(input_list[i][k], i, k) for i in range(N) for k in range(M)]
+    H.sort(reverse=True)
+
+    DX = [0,0,1,-1]
+    DY = [1,-1,0,0]
+
+    for h,i,k in H:
+        if h == 0:
+            continue
+        else:
+            for d in range(4):
+                ni, nk = i + DX[d], k + DY[d]
+                if 0 <= ni < N and 0 <= nk < M and input_list[i][k] > input_list[ni][nk]:
+                    memo[ni][nk] += memo[i][k]
+    
+    print(memo[N-1][M-1])
+
+sol()
