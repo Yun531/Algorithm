48c56672dc5ffbcc84bd3058d21b70dbafce12cc
김재윤 <74465803+Yun531@users.noreply.github.com>
2025-10-14 14:44:39 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 68 ms, Memory: 34908 KB -BaekjoonHub

---
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
@@ -0,0 +1,23 @@
+# https://www.acmicpc.net/problem/11866
+
+import sys
+input = sys.stdin.readline
+
+from collections import deque
+def sol():
+    data = list(map(int, input().split()))
+
+    que = deque()
+    for i in range(1,data[0]+1):
+        que.append(i)
+
+    result = "<"
+    while len(que) != 1:
+        for _ in range(data[1]-1):
+            que.append(que.popleft())
+        result = result + str(que.popleft()) + ', '
+
+    result = result + str(que.popleft()) + '>'
+    print(result)
+
+sol()
