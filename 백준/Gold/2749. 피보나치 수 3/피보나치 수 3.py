26fa0ea1f7a5d209b6035b3ed04d5ae0d0dea218
김재윤 <74465803+Yun531@users.noreply.github.com>
2025-10-15 11:06:28 +0900

    [Gold II] Title: 피보나치 수 3, Time: 312 ms, Memory: 92256 KB -BaekjoonHub

---
+++ 백준/Gold/2749. 피보나치 수 3/피보나치 수 3.py
@@ -0,0 +1,25 @@
+# https://www.acmicpc.net/problem/2749
+
+# https://kyun2da.github.io/2020/08/30/fibonacci/
+# https://yuuki0930.tistory.com/121
+
+# 피보나치 수열 성질들
+# https://www.acmicpc.net/blog/view/28
+
+import sys
+input = sys.stdin.readline
+
+def sol():
+    N = int(input())
+    period = 15*10**5         # 주기의 길이가 P 일 때, N번째 피보나치 수를 M으로 나눈 나머지는 N%P번째 피보나치 수를 M을 나눈 나머지와 동일
+
+    dp = [0]*(period+1)
+    dp[1] = 1
+
+    for i in range(2, period+1):
+        dp[i] = (dp[i-2] + dp[i-1]) % 10**6
+    
+    print(dp[N % period])
+
+sol()
+    
