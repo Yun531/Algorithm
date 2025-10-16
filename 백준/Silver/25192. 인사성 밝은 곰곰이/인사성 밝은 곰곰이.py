cc4f1dbd8fad3453695ae6c8cce6b426bcfb3fd6
김재윤 <74465803+Yun531@users.noreply.github.com>
2025-10-16 11:02:47 +0900

    Add alternative solution using set for duplicates
    Refactor solution to use set for user tracking.

--- 백준/Silver/25192. 인사성 밝은 곰곰이/인사성 밝은 곰곰이.py
+++ 백준/Silver/25192. 인사성 밝은 곰곰이/인사성 밝은 곰곰이.py
@@ -3,6 +3,9 @@
 # 딕셔너리
 # https://www.acmicpc.net/problem/25192
 
+# set
+# https://wikidocs.net/1015
+
 import sys
 input = sys.stdin.readline
 
@@ -19,6 +22,21 @@ def sol():
             users[name] = True
             count += 1
     
+    print(count)
+
+def sol_2():               # 단순 중복 여부만 확인할 것이기 때문에, set 이 더 적합함
+    N = int(input())
+    count = 0
+    users = set()
+
+    for _ in range(N):
+        name = input().rstrip()
+        if name == 'ENTER':
+            users.clear()
+        elif name not in users:
+            users.add(name)
+            count += 1
+
     print(count)
 
 sol()
