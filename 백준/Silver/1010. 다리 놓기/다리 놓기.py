7c0dede31f4fde9c5506b6955b9df7a9fa46f7d2
김재윤 <74465803+Yun531@users.noreply.github.com>
2025-10-16 10:37:46 +0900

    [Silver V] Title: 다리 놓기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.py
@@ -0,0 +1,20 @@
+# https://www.acmicpc.net/problem/1010
+
+import sys
+input = sys.stdin.readline
+
+def sol():
+    N = int(input())
+
+    for _ in range(N):
+        vals = list(map(int, input().split()))
+        print( fac(vals[1]) // (fac(vals[1]-vals[0]) * fac(vals[0]) ) )
+
+
+def fac(n):
+    result = 1
+    for i in range(1, n+1):
+        result *= i
+    return result
+
+sol()
