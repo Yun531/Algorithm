173c05fd8bd3e9c00b7b46270ff7c936d8ae4af8
김재윤 <74465803+Yun531@users.noreply.github.com>
2025-10-28 23:15:20 +0900

    [Bronze II] Title: 나머지, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/3052. 나머지/나머지.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+def sol():
+    result = set()
+    for _ in range(10):
+        n = int(input())
+        result.add(n % 42)
+    print(len(result))
+
+sol()
